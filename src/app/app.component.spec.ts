f8b8feb647494c1df1dc6e30fb868109a1a5d9cc
dreamerkumar <connectwithvkumar@gmail.com>
2025-01-27 23:34:20 -0500

    clean up

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-d3-samples');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-d3-samples');
-  });
 });
